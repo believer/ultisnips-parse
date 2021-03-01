7b6451578be1d9f3743cf937b57addf1f3334369
Rickard Natt och Dag <rickard@hey.com>
2021-03-01 08:43:25 +0100

    feat(parser): translate snippet id to name

--- packages/parser/api/languages.ts
+++ packages/parser/api/languages.ts
@@ -6,12 +6,36 @@ interface Snippet {
   name: string
 }
 
+const languageName = {
+  elixir: 'Elixir',
+  elm: 'Elm',
+  eruby: 'ERuby',
+  gitcommit: 'Git Commit',
+  javascript: 'JavaScript',
+  javascriptreact: 'JavaScript React',
+  rails: 'Ruby on Rails',
+  reason: 'ReasonML',
+  rescript: 'ReScript',
+  ruby: 'Ruby',
+  rust: 'Rust',
+  snippets: 'Ultisnips snippets',
+  typescript: 'TypeScript',
+  typescriptreact: 'TypeScript React',
+}
+
 const handler = async (_req: NowRequest, res: NowResponse) => {
   const response = await fetch(
     'https://api.github.com/repos/believer/dotfiles/contents/coc/ultisnips'
   )
   const data: Snippet[] = await response.json()
-  const languages = data.map(({ name }) => name.replace('.snippets', ''))
+  const languages = data.map(({ name }) => {
+    const id = name.replace('.snippets', '') as keyof typeof languageName
+
+    return {
+      id,
+      name: languageName[id] ?? id,
+    }
+  })
 
   res.json(languages)
 }
