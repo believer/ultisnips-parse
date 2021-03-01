7c860b41904493c1a83545b319bd7ae27ff91b27
Rickard Natt och Dag <rickard@hey.com>
2021-03-01 08:21:21 +0100

    feat(parser): get list of languages

---
+++ packages/parser/api/languages.ts
@@ -0,0 +1,21 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import fetch from 'node-fetch'
+import { allowCors } from '../utils/allowCors'
+
+interface Snippet {
+  name: string
+}
+
+const handler = async (_req: NowRequest, res: NowResponse) => {
+  const response = await fetch(
+    'https://api.github.com/repos/believer/dotfiles/contents/coc/ultisnips'
+  )
+  const data: Snippet[] = await response.json()
+  const languages = data.map(({ name }) => name.replace('.snippets', ''))
+
+  res.json(languages)
+}
+
+handler({} as any, {} as any)
+
+export default allowCors(handler)
